6641348a9bcebe087d7e571fddfd641513e16e79
Jaiber Restrepo Vila <104040403+jarvs10@users.noreply.github.com>
2025-02-10 10:22:48 -0500

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
             },
           });
         }}
-        className="w-full flex flex-row gap-2 items-center h-full"
+        className="w-full flex flex-row gap-2 justify-center items-center h-full"
       >
         <input
           type="text"
@@ -45,19 +45,19 @@ export default function Home() {
           value={input}
           disabled={isLoading}
           onChange={handleInputChange}
-          className="border-b border-dashed outline-none w-full px-4 py-2 text-[#0842A0] placeholder:text-[#0842A099] text-right focus:placeholder-transparent disabled:bg-transparent"
+          className="border-b rounded-md overflow-hidden border-dashed outline-none w-[850px] px-4 py-2 focus:placeholder-transparent disabled:bg-transparent"
         />
         <button
           type="submit"
-          className="rounded-full shadow-md border flex flex-row"
+          className="rounded-full shadow-md border flex flex-row bg-white hover:bg-sky-600"
         >
           {isLoading ? (
             <Loader2
               onClick={stop}
-              className="p-3 h-10 w-10 stroke-stone-500 animate-spin"
+              className="p-3 h-10 w-10 stroke-stone-800 animate-spin"
             />
           ) : (
-            <Send className="p-3 h-10 w-10 stroke-sky-700" />
+            <Send className="p-3 h-10 w-10" />
           )}
         </button>
       </form>
@@ -75,15 +75,15 @@ export default function Home() {
             <div
               key={index}
               className={`p-4 shadow-md rounded-md ml-10 relative ${
-                m.role === "user" ? "bg-stone-300 text-black" : "text-white"
+                m.role === "user" ? "bg-white text-black" : "text-white"
               }`}
             >
               <Markdown text={m.content} />
               {m.role === "user" ? (
-                <User2 className="absolute top-2 -left-10 border rounded-full p-1 shadow-lg" />
+                <User2 className="absolute bg-white top-2 -left-10 border rounded-full p-1 shadow-lg" />
               ) : (
                 <Bot
-                  className={`absolute top-2 -left-10 border rounded-full p-1 shadow-lg stroke-[#0842A0] ${
+                  className={`absolute bg-white top-2 -left-10 border rounded-full p-1 shadow-lg stroke-[#0842A0] ${
                     isLoading && index === messages.length - 1
                       ? "animate-bounce"
                       : ""
